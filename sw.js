6553fa92d80619a3f6ea580a7100d03077a0fdcc
Wahyu Rahmat Ilahi <wahyurahmat561@gmail.com>
2026-05-21 16:01:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
 const CACHE_NAME = 'kaskkn-v1';
 const urlsToCache = [
   './',
